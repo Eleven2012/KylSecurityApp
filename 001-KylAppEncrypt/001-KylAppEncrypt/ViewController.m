ab8c39e821394ba1aa2313b79e1d54d150297394
eleven 2011 <282889543@qq.com>
2019-12-14 18:01:27 +0800

    增加加密算法

--- 001-KylAppEncrypt/001-KylAppEncrypt/ViewController.m
+++ 001-KylAppEncrypt/001-KylAppEncrypt/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "KEncryptionTool.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self testEncrpytion];
+    
+}
+
+- (void) testEncrpytion {
+    /** AES - ECB */
+    NSString * key = @"abc";
+    NSString * encStr = [[KEncryptionTool shared] encryptString:@"hello" keyString:key iv:nil];
+    
+    NSLog(@"加密的结果是：%@",encStr);
+    
+    NSLog(@"解密的结果是：%@",[[KEncryptionTool shared] decryptString:encStr keyString:key iv:nil]);
+    
+    /** AES - CBC 加密 */
+    uint8_t iv[8] = {1,2,3,4,5,6,7,8};
+    NSData * ivData = [NSData dataWithBytes:iv length:sizeof(iv)];
+    
+    
+    
+    NSLog(@"CBC加密：%@",[[KEncryptionTool shared] encryptString:@"hello" keyString:@"abc" iv:ivData]);
+    
+    NSLog(@"解密：%@",[[KEncryptionTool shared] decryptString:@"u3W/N816uzFpcg6pZ+kbdg==" keyString:key iv:ivData]);
 }
 
 
